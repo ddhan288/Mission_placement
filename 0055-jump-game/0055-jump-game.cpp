bd47e7ee2163759c3123743ef02c4480f611199b
Deepak Dhan <31739855+ddhan288@users.noreply.github.com>
2023-07-19 21:47:21 +0530

    Time: 72 ms (66.97%), Space: 48.4 MB (73.44%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,25 +1,39 @@
+// class Solution {
+// public:
+//     bool canJump(vector<int>& nums) {
+        
+//         int maxReachable = nums[0];
+//         int i =1;
+        
+//         while ( i < nums.size() && maxReachable >= i  ){
+//             if (i + nums[i] > maxReachable ){
+//                 maxReachable = i + nums[i];
+//             }
+//             i++;
+            
+//         }
+//         if (maxReachable >= nums.size()-1){
+//             return true;
+//         }
+//         else 
+//             return false;
+            
+//         }
+    
+// };
+
+
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        
-        int maxReachable = nums[0];
-        int i =1;
-        
-        while ( i < nums.size() && maxReachable >= i  ){
-            if (i + nums[i] > maxReachable ){
-                maxReachable = i + nums[i];
-            }
-            i++;
-            
-        }
-        if (maxReachable >= nums.size()-1){
-            return true;
-        }
-        else 
-            return false;
-            
-        }
-    
+    int n = nums.size();
+    int maxReach = 0;
+    for (int i = 0; i < n; i++) {
+        if (i > maxReach) return false;
+        maxReach = max(maxReach, i + nums[i]);
+    }
+    return true;
+}
 };
 
 
