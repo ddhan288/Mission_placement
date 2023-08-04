47f36f908d9636633ce5d8df2c769d55272607c8
Deepak Dhan <31739855+ddhan288@users.noreply.github.com>
2023-08-04 23:29:41 +0530

    Time: 144 ms (64.13%), Space: 73.5 MB (7.54%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,36 +1,38 @@
-//using hashmap optimised version 
-//time complexity O(n)
-//space complexity 
-// class Solution {
-// public:
-//     bool containsDuplicate(vector<int>& nums) {
-//         map<int, int >M;
-//         for(auto i: nums){
-//             M[i]++;
-//         }
-//         for ( auto i: M){
-//             if ( i.second>1)
-//                 return 1;
-//         }
-//         return 0;
-//     }
-    
-// };
-
-//sorting 
+// using hashmap optimised version 
+// time complexity O(n)
+// space complexity 
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for (int i=0;i<nums.size()-1; i++){
-            if (nums[i] == nums[i+1])
-                return 1;
-            
+        map<int, int >M;
+        for(auto i: nums){
+            M[i]++;
         }
-        return false;
-        
+        for ( auto i: M){
+            if ( i.second>1)
+                return 1;
+        }
+        return 0;
     }
     
 };
+
+// //sorting
+// // Time complexity: O(nlogn)
+// // Space complexity: O(1)
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         for (int i=0;i<nums.size()-1; i++){
+//             if (nums[i] == nums[i+1])
+//                 return 1;
+            
+//         }
+//         return false;
+        
+//     }
+    
+// };
 
 
