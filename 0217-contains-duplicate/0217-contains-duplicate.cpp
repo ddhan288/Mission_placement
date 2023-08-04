6a68772a3e7d96911f018f82375b0d507076a687
Deepak Dhan <31739855+ddhan288@users.noreply.github.com>
2023-08-04 23:27:41 +0530

    Time: 95 ms (96.57%), Space: 57.3 MB (72.85%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,36 @@
+//using hashmap optimised version 
+//time complexity O(n)
+//space complexity 
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         map<int, int >M;
+//         for(auto i: nums){
+//             M[i]++;
+//         }
+//         for ( auto i: M){
+//             if ( i.second>1)
+//                 return 1;
+//         }
+//         return 0;
+//     }
+    
+// };
+
+//sorting 
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for (int i=0;i<nums.size()-1; i++){
+            if (nums[i] == nums[i+1])
+                return 1;
+            
+        }
+        return false;
+        
+    }
+    
+};
+
+
