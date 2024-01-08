56cc7968839c00337f0fbb7b6e42c7bdfecc71a5
Deepak Dhan <31739855+ddhan288@users.noreply.github.com>
2024-01-08 22:17:30 +0530

    Time: 61 ms (16.17%), Space: 41.9 MB (44.28%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    // declarative progaramming 
+    // return arr.filter(fn);
+    
+    
+    // imperative programming 
+    const res = [];
+    
+    for (let i = 0; i< arr.length; i++ ){
+        if (fn(arr[i], i)){
+            res.push(arr[i]);
+        }
+    }
+    
+    return res;
+};
