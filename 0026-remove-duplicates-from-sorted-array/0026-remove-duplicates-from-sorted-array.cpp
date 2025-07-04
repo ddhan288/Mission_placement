ea97217707973e54555a582eb1712e0ea59c58c9
Deepak Dhan <31739855+ddhan288@users.noreply.github.com>
2025-07-04 08:08:23 +0530

    Time: 0 ms (100%), Space: 22.5 MB (78.57%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,14 +1,39 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int i = 0; 
-        while (i < nums.size()-1){
-            if(nums[i] == nums[i+1]){
-                nums.erase(nums.begin() + i +1);
-            }else{
-                i++;
+        if (nums.empty())return 0;
+        int left = 1;
+        for(int right =1; right < nums.size(); right++){
+            if(nums[right] == nums[right -1]){
+                continue;
             }
+            nums[left] = nums[right];
+            left++;
         }
-        return nums.size();
+        return left;
+
     }
 };
+
+
+            
+
+        
+
+
+//brute foroce solution 
+//time Complexity O(n^2)
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+//         int i = 0; 
+//         while (i < nums.size()-1){// O(n)
+//             if(nums[i] == nums[i+1]){
+//                 nums.erase(nums.begin() + i +1);//O(n)
+//             }else{
+//                 i++;
+//             }
+//         }
+//         return nums.size();
+//     }
+// };
