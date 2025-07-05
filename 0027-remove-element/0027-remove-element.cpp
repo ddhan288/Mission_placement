9033678b60e9c72f644d94c3d5eceea3c27a0e2d
Deepak Dhan <31739855+ddhan288@users.noreply.github.com>
2025-07-05 08:42:17 +0530

    Time: 0 ms (100%), Space: 11.8 MB (5.18%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int n = nums.size();
-        sort(nums.begin(),nums.end());
-        for(int i =0; i<n; i++){
-            if(nums[i] == val){
-                
-                // nums.erase(remove(nums.begin(), nums.end(), val), nums.end());
-                auto iterator= remove(nums.begin(),nums.end(),val);
-                nums.erase(iterator,nums.end());
+        int size = 0;
+        for(int i =0; i< nums.size(); i++){
+            if(nums[i] != val){
+                nums[size] = nums[i];
+                size++;
             }
-
         }
-        int k = nums.size();
-        return k ;
+        return size;
     }
 };
+
+
+
+
+
+
+//Brute Force O(n^2) 
+// class Solution {
+// public:
+//     int removeElement(vector<int>& nums, int val) {
+//         int n = nums.size();
+//         sort(nums.begin(),nums.end());
+//         for(int i =0; i<n; i++){
+//             if(nums[i] == val){
+                
+//                 // nums.erase(remove(nums.begin(), nums.end(), val), nums.end());
+//                 auto iterator= remove(nums.begin(),nums.end(),val);
+//                 nums.erase(iterator,nums.end());
+//             }
+
+//         }
+//         int k = nums.size();
+//         return k ;
+//     }
+// };
