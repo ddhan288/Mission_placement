326149d757253867715240d8e097ecf37d793c47
Deepak Dhan <31739855+ddhan288@users.noreply.github.com>
2025-07-05 09:24:51 +0530

    Time: 0 ms (100%), Space: 11.7 MB (5.55%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+      reverse(digits.begin(), digits.end());
+      int one = 1; int i =0;
+
+      while(one){
+            if(i< digits.size()){
+                if(digits[i] == 9){
+                digits[i] =0;
+
+                }
+                else{
+                    digits[i]+=1;
+                    one = 0;
+                }   
+            }else{
+                digits.push_back(1);
+                one =0;
+            }
+            i++;
+
+        
+         }
+         reverse(digits.begin(), digits.end());  // Step 2: reverse back to original order
+        return digits;
+
+    }
+};
